7b0d84c1686334f6a967ef9b09b8b61a27bdca0c
Filemon Kepler <keplerdani@gmail.com>
2023-06-16 01:21:38 +0200

    remove placeholder codes

--- src/App.js
+++ src/App.js
@@ -7,25 +7,21 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: "Linda",
-          id: 1,
-        },
-        {
-          name: "Frank",
-          id: 2,
-        },
-        {
-          name: "Jacky",
-          id: 3,
-        },
-        {
-          name: "Andrei",
-          id: 4,
-        },
-      ],
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(() => {
+          return { monsters: users };
+        },
+        () => {
+          console.log(this.state)
+        })
+      );
   }
 
   render() {
