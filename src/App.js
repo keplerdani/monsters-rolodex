ca224d6e5115ca6c9e2dccab7ddbae2e1eaafb9a
Filemon Kepler <keplerdani@gmail.com>
2023-06-15 16:38:33 +0200

    add functionality to map array to elements

--- src/App.js
+++ src/App.js
@@ -6,11 +6,32 @@ class App extends Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      monsters: [
+        {
+          name: "Linda",
+        },
+        {
+          name: "Frank",
+        },
+        {
+          name: "Jacky",
+        },
+        {
+          name: "Andrei",
+        },
+      ],
+    };
   }
 
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => {
+          return <h1>{monster.name}</h1>;
+        })}
+      </div>
+    );
   }
 }
 export default App;
